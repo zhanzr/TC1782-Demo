8e6f95912d990b2cd0f7d22eca9a969d1cb524fa
zhanzr <zhanzr@foxmail.com>
2018-10-20 20:56:56 +0800

    Dhrystone in Flash and SPRAM test.

--- README.md
+++ README.md
@@ -6,13 +6,11 @@ Content:
 
 	Demo for Dhrystone benchmark.
 
-	Tricore 1782 @CPU:180 MHz Core:0131
-
-Dhrystone Benchmark, Version 2.1 (Language: C)
+Benchmark code in .text section:
 
 Program compiled with 'register' attribute
 
-Please give the number of runs through the benchmark: 
+Please give the number of runs through the benchmark:
 Execution starts, 600000 runs through Dhrystone
 Execution ends
 
@@ -65,7 +63,69 @@ Str_1_Loc:           DHRYSTONE PROGRAM, 1'ST STRING
 Str_2_Loc:           DHRYSTONE PROGRAM, 2'ND STRING
         should be:   DHRYSTONE PROGRAM, 2'ND STRING
 
-Microseconds for one run through Dhrystone[14-2326]:     3.9 
-Dhrystones per Second:                      259515.6 
+Microseconds for one run through Dhrystone[14-2601]:     2.6 
+Dhrystones per Second:                      386548.1 
 
+Benchmark code in PSPR:
+Tricore 1782 @CPU:180 MHz Core:0131
+
+Dhrystone Benchmark, Version 2.1 (Language: C)
+
+Program compiled with 'register' attribute
+
+Please give the number of runs through the benchmark: 
+Execution starts, 1000000 runs through Dhrystone
+Execution ends
+
+Final values of the variables used in the benchmark:
+
+Int_Glob:            5
+        should be:   5
+Bool_Glob:           1
+        should be:   1
+Ch_1_Glob:           A
+        should be:   A
+Ch_2_Glob:           B
+        should be:   B
+Arr_1_Glob[8]:       7
+        should be:   7
+Arr_2_Glob[8][7]:    1000010
+        should be:   Number_Of_Runs + 10
+Ptr_Glob->
+  Ptr_Comp:          -805278496
+        should be:   (implementation-dependent)
+  Discr:             0
+        should be:   0
+  Enum_Comp:         2
+        should be:   2
+  Int_Comp:          17
+        should be:   17
+  Str_Comp:          DHRYSTONE PROGRAM, SOME STRING
+        should be:   DHRYSTONE PROGRAM, SOME STRING
+Next_Ptr_Glob->
+  Ptr_Comp:          -805278496
+        should be:   (implementation-dependent), same as above
+  Discr:             0
+        should be:   0
+  Enum_Comp:         1
+        should be:   1
+  Int_Comp:          18
+        should be:   18
+  Str_Comp:          DHRYSTONE PROGRAM, SOME STRING
+        should be:   DHRYSTONE PROGRAM, SOME STRING
+Int_1_Loc:           5
+        should be:   5
+Int_2_Loc:           13
+        should be:   13
+Int_3_Loc:           7
+        should be:   7
+Enum_Loc:            1
+        should be:   1
+Str_1_Loc:           DHRYSTONE PROGRAM, 1'ST STRING
+        should be:   DHRYSTONE PROGRAM, 1'ST STRING
+Str_2_Loc:           DHRYSTONE PROGRAM, 2'ND STRING
+        should be:   DHRYSTONE PROGRAM, 2'ND STRING
+
+Microseconds for one run through Dhrystone[14-3063]:     3.0 
+Dhrystones per Second:                      327976.4 
 
--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 
 #include "dhry.h"
 
-#define RUN_NUMBER	600000
+#define RUN_NUMBER	1000000
 
 extern void __CSA_BEGIN(void);
 extern void __CSA(void);
@@ -124,13 +124,6 @@ void print_csa_reg(void)
 	_dsync();
 }
 
-#pragma section ".internalcode" ax
-void test_func_spram_1(void)
-{
-	printf("%s @ %08X\n", __func__, test_func_spram_1);
-}
-#pragma section
-
 const uint32_t GetFreeRTOSRunTimeTicks(void);
 
 inline void HAL_Delay(uint32_t m)
@@ -236,6 +229,9 @@ float           Microseconds,
 Dhrystones_Per_Second;
 /* end of variables for time measurement */
 
+//#pragma section ".text" ax
+#pragma section ".internalcode" ax
+
 void Proc_1 (Rec_Pointer Ptr_Val_Par)
 /******************/
 /* executed once */
@@ -319,9 +315,161 @@ void Proc_5 (void) /* without parameters */
 	Bool_Glob = false;
 } /* Proc_5 */
 
-inline void dhry_benchmark(void) __attribute__((always_inline));
 
-inline void dhry_benchmark(void)
+void Proc_6 (Enumeration Enum_Val_Par, Enumeration *Enum_Ref_Par)
+/*********************************/
+    /* executed once */
+    /* Enum_Val_Par == Ident_3, Enum_Ref_Par becomes Ident_2 */
+{
+  *Enum_Ref_Par = Enum_Val_Par;
+  if (! Func_3 (Enum_Val_Par))
+    /* then, not executed */
+    *Enum_Ref_Par = Ident_4;
+  switch (Enum_Val_Par)
+  {
+    case Ident_1:
+      *Enum_Ref_Par = Ident_1;
+      break;
+    case Ident_2:
+      if (Int_Glob > 100)
+        *Enum_Ref_Par = Ident_1;
+      else
+        *Enum_Ref_Par = Ident_4;
+      break;
+    case Ident_3: /* executed */
+      *Enum_Ref_Par = Ident_2;
+       break;
+    case Ident_4:
+       break;
+    case Ident_5:
+       *Enum_Ref_Par = Ident_3;
+       break;
+    } /* switch */
+} /* Proc_6 */
+
+
+void Proc_7 (One_Fifty Int_1_Par_Val,
+             One_Fifty Int_2_Par_Val,
+             One_Fifty *Int_Par_Ref )
+/**********************************************/
+    /* executed three times                                      */
+    /* first call:      Int_1_Par_Val == 2, Int_2_Par_Val == 3,  */
+    /*                  Int_Par_Ref becomes 7                    */
+    /* second call:     Int_1_Par_Val == 10, Int_2_Par_Val == 5, */
+    /*                  Int_Par_Ref becomes 17                   */
+    /* third call:      Int_1_Par_Val == 6, Int_2_Par_Val == 10, */
+    /*                  Int_Par_Ref becomes 18                   */
+{
+  One_Fifty Int_Loc;
+
+  Int_Loc = Int_1_Par_Val + 2;
+  *Int_Par_Ref = Int_2_Par_Val + Int_Loc;
+} /* Proc_7 */
+
+
+void Proc_8 (Arr_1_Dim Arr_1_Par_Ref,
+             Arr_2_Dim Arr_2_Par_Ref,
+             int Int_1_Par_Val,
+             int Int_2_Par_Val)
+/*********************************************************************/
+    /* executed once      */
+    /* Int_Par_Val_1 == 3 */
+    /* Int_Par_Val_2 == 7 */
+{
+  REG One_Fifty Int_Index;
+  REG One_Fifty Int_Loc;
+
+  Int_Loc = Int_1_Par_Val + 5;
+  Arr_1_Par_Ref [Int_Loc] = Int_2_Par_Val;
+  Arr_1_Par_Ref [Int_Loc+1] = Arr_1_Par_Ref [Int_Loc];
+  Arr_1_Par_Ref [Int_Loc+30] = Int_Loc;
+  for (Int_Index = Int_Loc; Int_Index <= Int_Loc+1; ++Int_Index)
+    Arr_2_Par_Ref [Int_Loc] [Int_Index] = Int_Loc;
+  Arr_2_Par_Ref [Int_Loc] [Int_Loc-1] += 1;
+  Arr_2_Par_Ref [Int_Loc+20] [Int_Loc] = Arr_1_Par_Ref [Int_Loc];
+  Int_Glob = 5;
+} /* Proc_8 */
+
+
+Enumeration Func_1 (Capital_Letter Ch_1_Par_Val, Capital_Letter Ch_2_Par_Val)
+/*************************************************/
+    /* executed three times                                         */
+    /* first call:      Ch_1_Par_Val == 'H', Ch_2_Par_Val == 'R'    */
+    /* second call:     Ch_1_Par_Val == 'A', Ch_2_Par_Val == 'C'    */
+    /* third call:      Ch_1_Par_Val == 'B', Ch_2_Par_Val == 'C'    */
+{
+  Capital_Letter        Ch_1_Loc;
+  Capital_Letter        Ch_2_Loc;
+
+  Ch_1_Loc = Ch_1_Par_Val;
+  Ch_2_Loc = Ch_1_Loc;
+  if (Ch_2_Loc != Ch_2_Par_Val)
+    /* then, executed */
+    return (Ident_1);
+  else  /* not executed */
+  {
+    Ch_1_Glob = Ch_1_Loc;
+    return (Ident_2);
+  }
+} /* Func_1 */
+
+
+Boolean Func_2 (Str_30  Str_1_Par_Ref, Str_30  Str_2_Par_Ref)
+/*************************************************/
+    /* executed once */
+    /* Str_1_Par_Ref == "DHRYSTONE PROGRAM, 1'ST STRING" */
+    /* Str_2_Par_Ref == "DHRYSTONE PROGRAM, 2'ND STRING" */
+{
+  REG One_Thirty      Int_Loc;
+  Capital_Letter      Ch_Loc;
+
+  Int_Loc = 2;
+  while (Int_Loc <= 2) /* loop body executed once */
+    if (Func_1 (Str_1_Par_Ref[Int_Loc],
+                Str_2_Par_Ref[Int_Loc+1]) == Ident_1)
+    /* then, executed */
+    {
+      Ch_Loc = 'A';
+      Int_Loc += 1;
+    } /* if, while */
+
+  if (Ch_Loc >= 'W' && Ch_Loc < 'Z')
+    /* then, not executed */
+    Int_Loc = 7;
+  if (Ch_Loc == 'R')
+    /* then, not executed */
+    return (true);
+  else /* executed */
+  {
+    if (strcmp (Str_1_Par_Ref, Str_2_Par_Ref) > 0)
+      /* then, not executed */
+    {
+      Int_Loc += 7;
+      Int_Glob = Int_Loc;
+      return (true);
+    }
+    else /* executed */
+      return (false);
+  } /* if Ch_Loc */
+} /* Func_2 */
+
+
+Boolean Func_3 (Enumeration Enum_Par_Val)
+/***************************/
+    /* executed once        */
+    /* Enum_Par_Val == Ident_3 */
+{
+  Enumeration Enum_Loc;
+
+  Enum_Loc = Enum_Par_Val;
+  if (Enum_Loc == Ident_3)
+    /* then, executed */
+    return (true);
+  else /* not executed */
+    return (false);
+} /* Func_3 */
+
+void dhry_benchmark(void)
 {
 	One_Fifty       Int_1_Loc;
 	REG One_Fifty   Int_2_Loc;
@@ -530,6 +678,8 @@ inline void dhry_benchmark(void)
 
 	}
 }
+
+#pragma section
 
 int main(void)
 {
